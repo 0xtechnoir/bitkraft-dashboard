637405293ba8e5aac30a7ba0b8c22d9d30d20f7a
0xNaylor <0xnaylor@gmail.com>
2022-07-22 15:28:45 +0100

    removed commented out code

--- src/pages/api/getTreasuryYieldCurves.js
+++ src/pages/api/getTreasuryYieldCurves.js
@@ -10,29 +10,8 @@ export default async function handler(req, res) {
 
         const data = await prisma.treasury_yield_curves.findMany();
 
-        // const series1 = data.map((element, index, array) => {
-        //     if (element["bc_10year"] == undefined || element["bc_3month"] == undefined) {
-        //         const val = parseFloat(array[index-1]["bc_10year"]) - parseFloat(array[index-1]["bc_3month"]) 
-
-        //         return {
-        //             "time" : element["new_date"],
-        //             "val" : val,
-        //         }
-        //     } else {
-        //         const val = parseFloat(element["bc_10year"]) - parseFloat(element["bc_3month"]) 
-
-        //         return {
-        //             "time" : element["new_date"],
-        //             "val" : val,
-        //         }
-        //     }  
-        // })
-
-        
-       
         const series1 = data.map((element, index, array) => {
 
-            // console.log(`Series 1 - Mapping Element: ${JSON.stringify(element)}`)
             Object.hasOwnProperty.bind(element)
 
             if (element.hasOwnProperty('bc_10year') || element.hasOwnProperty('bc_3month')) {
@@ -43,30 +22,21 @@ export default async function handler(req, res) {
                     "time" : element["new_date"],
                     "val" : val,
                 }
-              
-            } 
-
-                // const val = parseFloat(array[index-1]["bc_10year"]) - parseFloat(array[index-1]["bc_3month"]) 
-
-                return
-            
+            }             
         })
 
         const series2 = data.map((element, index, array) => {
-            // console.log(`Series 2 - Mapping Element: ${JSON.stringify(element)}`)
+
             Object.hasOwnProperty.bind(element)
             if (element.hasOwnProperty('bc_10year') || element.hasOwnProperty('bc_2year')) {
+                
                 const val = parseFloat(element["bc_10year"]) - parseFloat(element["bc_2year"])
 
                 return {
                     "time" : element["new_date"],
                     "val" : val,
                 }
-            } 
-                // const val = parseFloat(array[index-1]["bc_10year"]) - parseFloat(array[index-1]["bc_2year"])
-
-                return 
-            
+            }    
         })
 
         const resultsObj = {
