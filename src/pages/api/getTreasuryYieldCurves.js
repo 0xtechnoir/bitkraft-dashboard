b901c8b95fdcf7295d5dc1a1eff0c3eb267d5fa8
0xNaylor <0xnaylor@gmail.com>
2022-07-22 13:29:50 +0100

    added another blank line

--- src/pages/api/getTreasuryYieldCurves.js
+++ src/pages/api/getTreasuryYieldCurves.js
@@ -6,8 +6,9 @@ export default async function handler(req, res) {
     console.log(`getTreasuryYieldCurves endpoint invoked`)
 
     try {
-        
+
         const data = await prisma.treasury_yield_curves.findMany();
+        
         const series1 = data.map((element, index, array) => {
             if (element["bc_10year"] == undefined || element["bc_3month"] == undefined) {
                 const val = array[index-1]["bc_10year"] - array[index-1]["bc_3month"] 
