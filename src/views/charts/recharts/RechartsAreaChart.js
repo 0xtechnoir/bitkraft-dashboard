102b39ef93ce3f22fd1e520a6d43ef7d61cb8007
0xNaylor <0xnaylor@gmail.com>
2022-08-25 21:06:24 +0100

    Aggregated open interest BTC futures chart v1

--- src/views/charts/recharts/RechartsAreaChart.js
+++ src/views/charts/recharts/RechartsAreaChart.js
@@ -1,5 +1,5 @@
 // ** React Imports
-import { forwardRef, useState } from 'react'
+import { forwardRef, useState, useEffect } from 'react'
 
 // ** MUI Imports
 import Box from '@mui/material/Box'
@@ -14,12 +14,14 @@ import InputAdornment from '@mui/material/InputAdornment'
 // ** Third Party Imports
 import format from 'date-fns/format'
 import DatePicker from 'react-datepicker'
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
+import { Legend, AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts'
 
 // ** Icons Imports
 import Circle from 'mdi-material-ui/Circle'
 import BellOutline from 'mdi-material-ui/BellOutline'
 import ChevronDown from 'mdi-material-ui/ChevronDown'
+
+const lineColours = ["#e6194B", "#3cb44b", "#ffe119", "#4363d8", "#f58231", "#911eb4", "#42d4f4", "#f032e6" , "#e6194B", "#3cb44b", "#ffe119", "#4363d8", "#f58231", "#911eb4", "#42d4f4", "#f032e6" ]
 
 const data = [
   {
@@ -128,10 +130,19 @@ const CustomTooltip = data => {
   return null
 }
 
-const RechartsAreaChart = ({ direction }) => {
+const RechartsAreaChart = (props, {direction}) => {
+  console.log(`${props.title} chart loaded`)
+  console.dir(props.data)
   // ** States
   const [endDate, setEndDate] = useState(null)
+  const [loaded, setLoaded] = useState(false)
   const [startDate, setStartDate] = useState(new Date())
+
+  useEffect(() => {
+    if (props.data) {
+      setLoaded(true)
+    }  
+  }, [props.data])
 
   const CustomInput = forwardRef((props, ref) => {
     const startDate = format(props.start, 'MM/dd/yyyy')
@@ -166,61 +177,139 @@ const RechartsAreaChart = ({ direction }) => {
     setEndDate(end)
   }
 
-  return (
+  function formatXAxis(value) {
+    let date = new Date(value)
+    const month = ("0" + (date.getMonth() + 1)).slice(-2)
+    return `${month}/${date.getFullYear()}`;
+  }
+
+  function formatYAxis(value) {
+    return abbreviate(value, 2, false, false)
+  }
+
+  function abbreviate(number, maxPlaces, forcePlaces, forceLetter) {
+    number = Number(number)
+    forceLetter = forceLetter || false
+    if(forceLetter !== false) {
+      return annotate(number, maxPlaces, forcePlaces, forceLetter)
+    }
+    var abbr
+    if(number >= 1e12) {
+      abbr = 'T'
+    }
+    else if(number >= 1e9) {
+      abbr = 'B'
+    }
+    else if(number >= 1e6) {
+      abbr = 'M'
+    }
+    else if(number >= 1e3) {
+      abbr = 'K'
+    }
+    else {
+      abbr = ''
+    }
+    return annotate(number, maxPlaces, forcePlaces, abbr)
+  }
+  
+  function annotate(number, maxPlaces, forcePlaces, abbr) {
+    // set places to false to not round
+    var rounded = 0
+    switch(abbr) {
+      case 'T':
+        rounded = number / 1e12
+        break
+      case 'B':
+        rounded = number / 1e9
+        break
+      case 'M':
+        rounded = number / 1e6
+        break
+      case 'K':
+        rounded = number / 1e3
+        break
+      case '':
+        rounded = number
+        break
+    }
+    if(maxPlaces !== false) {
+      var test = new RegExp('\\.\\d{' + (maxPlaces + 1) + ',}$')
+      if(test.test(('' + rounded))) {
+        rounded = rounded.toFixed(maxPlaces)
+      }
+    }
+    if(forcePlaces !== false) {
+      rounded = Number(rounded).toFixed(forcePlaces)
+    }
+    return rounded + abbr
+  }
+
+  if (loaded) {
+
+    return (
+      <Card>
+        <CardHeader
+          title='Aggregated Open Future Interest of Bitcoin Futures'
+          titleTypographyProps={{ variant: 'h6' }}
+          sx={{
+            flexDirection: ['column', 'row'],
+            alignItems: ['flex-start', 'center'],
+            '& .MuiCardHeader-action': { mb: 0 },
+            '& .MuiCardHeader-content': { mb: [2, 0] }
+          }}
+        />
+        <CardContent>
+          <Box sx={{ height: 500 }}>
+            <ResponsiveContainer>
+              <AreaChart height={350} margin={{ left: -20 }} data={props.data} >
+                <CartesianGrid />
+                <XAxis dataKey='date' tickFormatter={formatXAxis} type="catagory" allowDuplicatedCategory={false}/>
+                <YAxis tickFormatter={formatYAxis}/>
+                <Area dataKey={Object.keys(props.data[0])[1]} stackId="1" stroke={lineColours[0]} fill={lineColours[0]} />
+                <Area dataKey={Object.keys(props.data[0])[2]} stackId="1" stroke={lineColours[1]} fill={lineColours[1]} />
+                <Area dataKey={Object.keys(props.data[0])[3]} stackId="1" stroke={lineColours[2]} fill={lineColours[2]} />
+                <Area dataKey={Object.keys(props.data[0])[4]} stackId="1" stroke={lineColours[3]} fill={lineColours[3]} />
+                <Area dataKey={Object.keys(props.data[0])[5]} stackId="1" stroke={lineColours[4]} fill={lineColours[4]} />
+                <Area dataKey={Object.keys(props.data[0])[6]} stackId="1" stroke={lineColours[5]} fill={lineColours[5]} />
+                <Area dataKey={Object.keys(props.data[0])[7]} stackId="1" stroke={lineColours[6]} fill={lineColours[6]} />
+                <Area dataKey={Object.keys(props.data[0])[8]} stackId="1" stroke={lineColours[7]} fill={lineColours[7]} />
+                <Area dataKey={Object.keys(props.data[0])[9]} stackId="1" stroke={lineColours[8]} fill={lineColours[8]} />
+                <Area dataKey={Object.keys(props.data[0])[10]} stackId="1" stroke={lineColours[9]} fill={lineColours[9]} />
+                <Area dataKey={Object.keys(props.data[0])[11]} stackId="1" stroke={lineColours[10]} fill={lineColours[10]} />
+                <Area dataKey={Object.keys(props.data[0])[12]} stackId="1" stroke={lineColours[11]} fill={lineColours[11]} />
+                <Area dataKey={Object.keys(props.data[0])[13]} stackId="1" stroke={lineColours[12]} fill={lineColours[12]} />
+                <Tooltip content={CustomTooltip}/>
+                <Legend verticalAlign="bottom" height={36}/>
+              </AreaChart>
+            </ResponsiveContainer>
+          </Box>
+        </CardContent>
+      </Card>
+    )
+
+  } else {
     <Card>
-      <CardHeader
-        title='Website Data'
-        titleTypographyProps={{ variant: 'h6' }}
-        sx={{
-          flexDirection: ['column', 'row'],
-          alignItems: ['flex-start', 'center'],
-          '& .MuiCardHeader-action': { mb: 0 },
-          '& .MuiCardHeader-content': { mb: [2, 0] }
-        }}
-        action={
-          <DatePicker
-            selectsRange
-            endDate={endDate}
-            id='recharts-area'
-            selected={startDate}
-            startDate={startDate}
-            onChange={handleOnChange}
-            placeholderText='Click to select a date'
-            customInput={<CustomInput start={startDate} end={endDate} />}
-          />
-        }
-      />
-      <CardContent>
-        <Box sx={{ display: 'flex', mb: 4 }}>
-          <Box sx={{ mr: 6, display: 'flex', alignItems: 'center' }}>
-            <Circle sx={{ mr: 1.5, fontSize: '0.75rem', color: 'rgb(115, 103, 240)' }} />
-            <Typography>Click</Typography>
-          </Box>
-          <Box sx={{ mr: 6, display: 'flex', alignItems: 'center' }}>
-            <Circle sx={{ mr: 1.5, fontSize: '0.75rem', color: 'rgba(115, 103, 240, .5)' }} />
-            <Typography>Sales</Typography>
-          </Box>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
-            <Circle sx={{ mr: 1.5, fontSize: '0.75rem', color: 'rgba(115, 103, 240, .2)' }} />
-            <Typography>Visits</Typography>
-          </Box>
-        </Box>
+        <CardHeader
+          title='Aggregated Open Future Interest of Bitcoin Futures'
+          titleTypographyProps={{ variant: 'h6' }}
+          sx={{
+            flexDirection: ['column', 'row'],
+            alignItems: ['flex-start', 'center'],
+            '& .MuiCardHeader-action': { mb: 0 },
+            '& .MuiCardHeader-content': { mb: [2, 0] }
+          }}
+        />
+        <CardContent>
         <Box sx={{ height: 350 }}>
-          <ResponsiveContainer>
-            <AreaChart height={350} data={data} style={{ direction }} margin={{ left: -20 }}>
-              <CartesianGrid />
-              <XAxis dataKey='name' reversed={direction === 'rtl'} />
-              <YAxis orientation={direction === 'rtl' ? 'right' : 'left'} />
-              <Tooltip content={CustomTooltip} />
-              <Area dataKey='Clicks' stackId='Clicks' stroke='0' fill='rgb(115, 103, 240)' />
-              <Area dataKey='Sales' stackId='Sales' stroke='0' fill='rgba(115, 103, 240, .5)' />
-              <Area dataKey='Visits' stackId='Visits' stroke='0' fill='rgba(115, 103, 240, .2)' />
-            </AreaChart>
+            <ResponsiveContainer>
+          <div>
+            <p>Loading</p>
+          </div>
           </ResponsiveContainer>
-        </Box>
-      </CardContent>
-    </Card>
-  )
+          </Box>
+        </CardContent>
+      </Card>
+  }
 }
 
 export default RechartsAreaChart
