633b8df49424b8679ecbfec8305213f571823a93
0xNaylor <0xnaylor@gmail.com>
2023-02-06 10:36:45 +0000

    Diasbled ESLint rules

--- next.config.js
+++ next.config.js
@@ -18,6 +18,11 @@ module.exports = withTM({
     esmExternals: false,
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
